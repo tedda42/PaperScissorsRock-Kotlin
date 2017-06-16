8f1f7dbfb5d29106b3bfd0b73a373d01f1022a48
tedda42 <tedda42@gmail.com>
2017-06-16 21:22:09 +1000

    Add PaperScissorsRock implementation and run script

---
+++ run.sh
@@ -0,0 +1,2 @@
+kotlinc src/com/github/tedda42/psr/MainKotlin.kt -include-runtime -d out/psr.jar
+java -jar out/psr.jar
--- src/com/github/tedda42/psr/MainKotlin.kt
+++ src/com/github/tedda42/psr/MainKotlin.kt
@@ -1,5 +1,52 @@
 package com.github.tedda42.psr
 
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
 fun main(args: Array<String>) {
-    println("Hello World!")
+    println("Do you choose rock, paper or scissors?")
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val userChoice = br.readLine()
+    val computerChoiceRand = Math.random()
+    val computerChoice: String
+
+    if (computerChoiceRand < 0.34) {
+        computerChoice = "rock"
+    } else if (computerChoiceRand <= 0.67) {
+        computerChoice = "paper"
+    } else {
+        computerChoice = "scissors"
+    }
+
+    println("User    : " + userChoice)
+    println("Computer: " + computerChoice)
+
+    val result = compare(userChoice, computerChoice)
+    println(result)
+}
+
+internal fun compare(choice1: String, choice2: String): String {
+    if (choice1 == choice2) {
+        return "The result is a tie!"
+    } else if (choice1 == "rock") {
+        if (choice2 == "scissors") {
+            return "rock wins!"
+        } else {
+            return "paper wins!"
+        }
+    } else if (choice1 == "paper") {
+        if (choice2 == "rock") {
+            return "paper wins!"
+        } else {
+            return "scissors wins!"
+        }
+    } else if (choice1 == "scissors") {
+        if (choice2 == "rock") {
+            return "rock wins!"
+        } else {
+            return "scissors wins!"
+        }
+    } else {
+        return "Invalid choice '$choice1'"
+    }
 }
